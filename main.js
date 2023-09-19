025fe55a98c0b7c6a0920e2d9836f7c95362beb5
Cadenna <109689063+Cadenna@users.noreply.github.com>
2023-09-18 18:20:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,28 @@ function validateForm(){
 
     return true;
 }
+
+//read
+function showData(){
+
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    }else{
+        listPeople = JSON.parse(localStorage.getItem("listPeople"));
+    }
+
+    var html = "";
+
+    listPeople.forEach(function(element, index){
+        html += "<tr>";
+        html += '<td class="col-md-1"; style="text-align: center"><button onclick="updateData('+ index +')" class="btn btn-outline-dark"><i class="bi bi-pencil-square"></i></i></button></td>';
+        html += '<td class="col-md-5">' + element.name + '</td>';
+        html += '<td class="col-md-3">' + element.date + '</td>';
+        html += '<td class="col-md-1"; style="text-align: center"><button onclick="deleteData('+ index +')" class="btn btn-outline-dark"><i class="bi bi-x-lg"></i></button></td>';
+        html += "</tr>";
+    });
+
+    document.querySelector('#tableData tbody').innerHTML = html;
+}
